2bcf6d49bf9cbaddddf61102719b256df78fd5c7
Lushen Moodley <lushenmoodley@yahoo.com>
2022-09-25 13:06:09 +0200

    Mvc areas upload

---
+++ UrlRouting/UrlRouting/Areas/Employees/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UrlRouting.Areas.Employees.Models;
+using UrlRouting.Areas.Sales.Models;
+
+namespace UrlRouting.Areas.Employees.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Area("Employees")]
+        public IActionResult Index()
+        {
+     
+            Workers[] employees = new Workers[]
+            {
+                new Workers{FullName="Lushen Moodley", Occupation="Software Developer", Salary=10.00},
+                new Workers {FullName="Zack Moodley", Occupation="Security Guard",Salary=20.00 },
+                new Workers{FullName="Kevin Jennings",Occupation="Scrum Master",Salary=30.00}
+            };
+
+            return View(employees);
+        }
+
+
+        
+        
+        }
+
+
+
+    }
+
---
+++ UrlRouting/UrlRouting/Areas/Sales/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UrlRouting.Areas.Sales.Models;
+
+namespace UrlRouting.Areas.Sales.Controllers
+{
+    [Area("Sales")]//It is used to associate a Controller with the Sales area.
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            Product[] productArray = new Product[] {
+                new Product { Name = "Pants", Quantity = 5, Price=100 },
+                new Product { Name = "Shirts", Quantity = 10, Price=80 },
+                new Product { Name = "Shoes", Quantity = 15, Price=50 }
+            };
+            return View(productArray);
+        }
+    }
+}
